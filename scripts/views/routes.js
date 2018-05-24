34d7afe2ccea289f4bd4ed205dedfd0a36e55861
Kevin Hwangpo <hwangpo3@gmail.com>
2018-05-24 13:31:02 -0700

    added icons.css information

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,6 +1,9 @@
 'use strict';
 
 page('/', (ctx) => app.Book.fetchAll(app.bookView.initIndexPage));
+
 page('/book/add', ctx => app.bookView.initAddForm(ctx));
+
 page('/book/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initdetailpage));
+
 page();
